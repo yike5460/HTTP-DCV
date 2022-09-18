51983bab8e4dc09b62a610600b2919243db100e2
yike5460 <yike5460@163.com>
2022-09-18 16:16:50 +0800

    first blood

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,46 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, aws_ec2, aws_ecs, aws_ecs_patterns} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+// import { AwsSolutionsChecks } from 'cdk-nag';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+    // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-ecs-patterns-readme.html
 
-    // define resources here...
+    // vpc for ecs cluster with fargate
+    const vpc = new aws_ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+    });
+
+    // ecs cluster with fargate
+    const cluster = new aws_ecs.Cluster(this, 'Cluster', {
+      clusterName: 'my-cluster',
+      vpc: vpc,
+    });
+
+    const loadBalancedFargateService = new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      cluster,
+      memoryLimitMiB: 1024,
+      desiredCount: 1,
+      cpu: 512,
+      taskImageOptions: {
+        image: aws_ecs.ContainerImage.fromAsset('src/containers'),
+      },
+    });
+
+    const scalableTarget = loadBalancedFargateService.service.autoScaleTaskCount({
+      minCapacity: 1,
+      maxCapacity: 20,
+    });
+
+    scalableTarget.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+    });
+
+    scalableTarget.scaleOnMemoryUtilization('MemoryScaling', {
+      targetUtilizationPercent: 50,
+    });
+
   }
 }
 
@@ -18,6 +53,6 @@ const devEnv = {
 const app = new App();
 
 new MyStack(app, 'projenDemo-dev', { env: devEnv });
-// new MyStack(app, 'projenDemo-prod', { env: prodEnv });
+// Aspects.of(app).add(new AwsSolutionsChecks({verbose:true}));
 
 app.synth();
